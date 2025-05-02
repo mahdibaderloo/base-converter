ebcc53f76e7783aea4f524dfaa4da9e2ae9ce752
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-02 16:58:09 +0330

    main template

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,26 @@
 function Main() {
-  return <div></div>;
+  return (
+    <main>
+      <div>
+        <p>From:</p>
+        <select>
+          <option value="Decimal">Decimal 10</option>
+          <option value="Binary">Binary 2</option>
+          <option value="Octal">Octal 8</option>
+          <option value="Hexadecimal">Hexadecimal 16</option>
+        </select>
+      </div>
+      <div>
+        <p>To:</p>
+        <select>
+          <option value="Binary">Binary 2</option>
+          <option value="Octal">Octal 8</option>
+          <option value="Hexadecimal">Hexadecimal 16</option>
+          <option value="Decimal">Decimal 10</option>
+        </select>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
