fb52b1fb681a54e6ae9c6edb63ad505b97891989
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-02 17:11:40 +0330

    main states

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Result from "./components/Result";
 
 function App() {
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 function Main({ children }) {
+  const [from, setFrom] = useState("Decimal");
+  const [to, setTo] = useState("Binary");
+  const [amount, setAmount] = useState(0);
+
   return (
     <main>
       <div>
         <p>From:</p>
-        <select>
+        <select onClick={(e) => setFrom(e.target.value)}>
           <option value="Decimal">Decimal 10</option>
           <option value="Binary">Binary 2</option>
           <option value="Octal">Octal 8</option>
@@ -12,15 +18,16 @@ function Main({ children }) {
       </div>
       <div>
         <p>To:</p>
-        <select>
+        <select onClick={(e) => setTo(e.target.value)}>
           <option value="Binary">Binary 2</option>
           <option value="Octal">Octal 8</option>
           <option value="Hexadecimal">Hexadecimal 16</option>
           <option value="Decimal">Decimal 10</option>
         </select>
       </div>
+      <input type="text" onChange={(e) => setAmount(e.target.value)} />
       {children}
-      <div>0101101</div>
+      <p>0101101</p>
     </main>
   );
 }
