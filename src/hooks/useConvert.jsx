71d983cf2ad72cff7d0c7c7a31a0928a050b7c59
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-03 13:09:37 +0330

    edit fromBinaryToDecimal function

--- src/hooks/useConvert.jsx
+++ src/hooks/useConvert.jsx
@@ -12,16 +12,16 @@ export default function useConvert(from, to, amount) {
     [amount, from, to, result]
   );
 
-  function fromBinaryToDecimal(amount) {
-    const amountArray = amount.split("");
-    let convertResult = [];
-    let j = 0;
-    for (let i = amountArray.length - 1; i >= 0; i--) {
-      convertResult.push(Number(amountArray[i]) * 2 ** j++);
-    }
-    const finalResult = convertResult.reduce((prev, cur) => (cur += prev));
-    return finalResult;
-  }
-
   return result;
+}
+
+function fromBinaryToDecimal(amount) {
+  const amountArray = amount.split("");
+  let convertResult = [];
+  let j = 0;
+  for (let i = amountArray.length - 1; i >= 0; i--) {
+    convertResult.push(Number(amountArray[i]) * 2 ** j++);
+  }
+  const finalResult = convertResult.reduce((prev, cur) => (cur += prev));
+  return finalResult;
 }
