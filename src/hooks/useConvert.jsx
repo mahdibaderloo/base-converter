ec805918d31537a306ed8961ecf837ebf157d31d
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-03 13:51:31 +0330

    octalToDecimal function

--- src/hooks/useConvert.jsx
+++ src/hooks/useConvert.jsx
@@ -5,9 +5,10 @@ export default function useConvert(from, to, amount) {
 
   useEffect(
     function () {
-      if (from === "Binary" && to === "Decimal" && amount !== "") {
-        setResult(fromBinaryToDecimal(amount));
-      }
+      if (from === "Binary" && to === "Decimal" && amount !== "")
+        setResult(binaryToDecimal(amount));
+      if (from === "Octal" && to === "Decimal" && amount !== "")
+        setResult(octalToDecimal(amount));
     },
     [amount, from, to]
   );
@@ -15,12 +16,23 @@ export default function useConvert(from, to, amount) {
   return { result };
 }
 
-function fromBinaryToDecimal(amount) {
+function binaryToDecimal(amount) {
   const amountArray = amount.split("");
   let convertResult = [];
   let j = 0;
   for (let i = amountArray.length - 1; i >= 0; i--) {
     convertResult.push(Number(amountArray[i]) * 2 ** j++);
+  }
+  const finalResult = convertResult.reduce((prev, cur) => prev + cur, 0);
+  return finalResult;
+}
+
+function octalToDecimal(amount) {
+  const amountArray = amount.split("");
+  let convertResult = [];
+  let j = 0;
+  for (let i = amountArray.length - 1; i >= 0; i--) {
+    convertResult.push(Number(amountArray[i]) * 8 ** j++);
   }
   const finalResult = convertResult.reduce((prev, cur) => prev + cur, 0);
   return finalResult;
