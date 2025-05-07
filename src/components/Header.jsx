780b480a327922545131db4f9b45d71e167a64cb
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-07 19:22:57 +0330

    edit image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="h-1/6 w-full flex items-center gap-2 mb-8">
-      <img src="..//convert-icon.svg" className="w-14" />
+      <img src="./convert-icon.svg" className="w-14" />
       <h1 className="text-cyan-300">Base Converter</h1>
     </header>
   );
