33e9c21b90bec3cce9e319ecd1760dee8d773cfa
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-07 19:06:34 +0330

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="bg-slate-900 w-72 h-10/12 rounded-2xl mx-auto p-2 text-slate-200 font-comic ">
+    <div className="bg-slate-900 w-full h-screen mx-auto p-3 text-slate-200 font-comic sm:w-96 sm:h-[800px] sm:rounded-2xl ">
       <Header />
       <Main />
     </div>
