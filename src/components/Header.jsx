ae85854a36aeaee87628a331333bfe605a2b479d
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-08 09:52:04 +0330

    import icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import icon from "../../public/convert-icon.svg";
+
 function Header() {
   return (
     <header className="h-1/6 w-full flex items-center gap-2 mb-8">
-      <img src="/convert-icon.svg" className="w-14" />
+      <img src={icon} className="w-14" />
       <h1 className="text-cyan-300">Base Converter</h1>
     </header>
   );
